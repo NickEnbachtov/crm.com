a5bf01a351d377c5fb81ce3746c9b8f3dd1f0476
Николай Енбахтов <enbahtov@gmail.com>
2015-11-11 23:10:58 +0600

    v.1.2

--- views/site/index.php
+++ views/site/index.php
@@ -5,6 +5,11 @@
 $this->title = 'index page';
 ?>
 <?php
-echo dirname(__DIR__);
+//echo dirname(__DIR__);
 ?>
+<div class="left-menu">
+<div class="btn-left-menu">Add task</div>
+</div>
+<div class="right-box">
+</div>
 
